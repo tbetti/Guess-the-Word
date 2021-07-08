ba437bb191d1590c77a0172ca75c2d6f4bb7a0e4
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-08 11:27:05 -0400

    Update wins and losses when user completes word or when time runs out

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,30 @@
 var timer = document.getElementById("timer");
 var startTimer = document.getElementById("start");
+var wins = 0;
+var losses = 0;
+var winText = document.getElementById("wins");
+var loseText = document.getElementById("losses");
 var availableWords = ["HELLO", "JAVASCRIPT", "OBJECT", "FUNCTION", "ELEMENT", "CASCADING", "STYLESHEET"];
 var secondsLeft = 10;
 var word = document.getElementById("guess-word");
 var splitWord = [];
 var blankLetters = [];
 
+
+winText.textContent = wins;
+loseText.textContent = losses;
+
+// When start button clicked, start timer and display word
+startTimer.addEventListener("click", beginGame);
+
+function beginGame() {
+    if (secondsLeft > 0) {
+        countdown();
+        pickRandomWord();
+    } else {
+        resetGame(); // works well
+    }
+}
 
 //Randomize words to display
 function pickRandomWord() {
@@ -23,26 +42,50 @@ function pickRandomWord() {
     word.append(blankLetters.join(" "));
 }
 
+// Each time a key is ipressed, change to uppercase and check if pressed key is in the word
+document.addEventListener("keydown", function (event) {
+    var guessLetter = event.key.toUpperCase();
+    console.log(guessLetter);
+
+    checkLetters(guessLetter);
+    wordComplete();
+})
+
+// Check to see if pressed letter is part of the mystery word
 function checkLetters(letter){
     console.log("Letter: " + letter);
     for(var i=0; i< splitWord.length; i++){
         if (splitWord[i] === letter){
                 blankLetters[i]=letter;
                 console.log("BlankLetters: " + blankLetters)
-            }   
-        }
+        }   
+    }
     word.textContent=blankLetters.join(" ");
 }
 
-document.addEventListener("keydown", function (event) {
-    // console.log(event.code);
-    var guessLetter = event.key.toUpperCase(); // const can't be reassigned
-    console.log(guessLetter);
-    var possibleCharactrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var characterArray = possibleCharactrs.split();
+// If word is complete, update win score and reset game
+function wordComplete(){
+    var userGuess = blankLetters.join("");
+    var actualWord = splitWord.join("");
 
-    checkLetters(guessLetter);
-})
+    if (userGuess === actualWord){
+        wins++;
+        winText.textContent = wins;
+
+        //resetGame(); //makes time go faster and doesn't stop at 0
+    }
+}
+
+// Reset arrays and timer
+function resetGame(){ 
+    splitWord = [];
+    blankLetters = [];
+    secondsLeft = 10;
+    timer.textContent = secondsLeft + " seconds";
+    word.innerHTML = "";
+
+    beginGame();
+}
 
 // Countdown time left
 function countdown() {
@@ -52,19 +95,8 @@ function countdown() {
         
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
+            losses++;
+            loseText.textContent = losses;
         }
     }, 1000);
 }
-
-// Start timer and display word when start button clicked
-startTimer.addEventListener("click", function () {
-    if (secondsLeft > 0) {
-        countdown();
-        pickRandomWord();
-    } else {
-        secondsLeft = 10;
-        timer.textContent = secondsLeft + " seconds";
-        countdown();
-        pickRandomWord();
-    }
-});
